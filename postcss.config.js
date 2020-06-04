a8fc2c0e48111441a9974b418d9f3f18d5c086dc
Luca Rossi <luca@wanderio.com>
2020-06-04 14:13:40 +0200

    Added purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
+const purgecss = require(`@fullhuman/postcss-purgecss`)
+
 module.exports = () => {
     return {
-        plugins: [require(`tailwindcss`)],
+        plugins: [
+            require(`tailwindcss`),
+            purgecss({
+                content: [`./layouts/**/*.html`, `./src/**/*.js`],
+                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+            }),
+        ],
     }
 }
